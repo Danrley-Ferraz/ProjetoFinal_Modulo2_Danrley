8a9f2c3511e1f42b405e5d9f33c188e67c8c6cc8
Danrley Ferraz <110141527+Danrley-Ferraz@users.noreply.github.com>
2023-09-16 01:40:23 -0300

    Atualização ADD Obstacle_manager / Obstacles / dinosaur

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -7,8 +7,33 @@ from dino_runner.components.obstacles.cactus import Cactus
 class ObstacleManager():
     def __init__(self):
         self.obstacles = []
-        self.when_appars = 0
     
-    def generate_cactus(self):
+    def update(self, game):
+        obstacle_type = [
+            Cactus(),
+            Bird()
+        ]
+
         if len(self.obstacles) == 0:
-            self.when_appars
+            self.obstacles.append(obstacle_type[random.randint(0, 1)])
+        
+        for obstacle in self.obstacles:
+            obstacle.update(game.game_speed, self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                if not game.player.has_power_up:
+                    pygame.time.delay(500)
+                    game.playing = False
+                    game.death_count += 1
+                    break
+                else:
+                    self.obstacles.remove(obstacle)
+
+    
+    def reset_obstacles(self):
+        self.obstacles = []
+    
+    def draw(self, screen):
+        for obstacle in self.obstacles:
+            obstacle.draw(screen)
+
+
--- dino_runner/components/obstacles/obstacles.py
+++ dino_runner/components/obstacles/obstacles.py
@@ -1,4 +1,4 @@
-import random 
+import pygame
 
 from pygame.sprite import Sprite
 from dino_runner.utils.constants import SCREEN_WIDTH
@@ -7,15 +7,22 @@ class Obstacles(Sprite):
     def __init__(self, image, type):
         self.image = image
         self.type = type
-        self.rect = self.image.get_rect()
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-
-
+    
     def update(self, game_speed, obstacles):        
-        self.rect.x -= game_speed       
+        self.rect.x -= game_speed     
+
         if self.rect.x < -self.rect.width:
             obstacles.pop()
             
+
+    def draw(self, screen):
+        screen.blit(self.image[self.type], (self.rect.x, self.rect.y))
+    
+    
+
+
+    
     
